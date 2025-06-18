f95a02f6aae24165bf52c17b88e2d2c7b2ab3d34
Leandro H <ndroher+git@proton.me>
2025-06-18 16:04:04 -0300

    feat(favoritos): adicionar lista de pokemons favoritos

--- src/app/pages/favoritos/favoritos.page.ts
+++ src/app/pages/favoritos/favoritos.page.ts
@@ -1,17 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import {
   IonHeader,
   IonToolbar,
   IonTitle,
   IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonAvatar,
 } from '@ionic/angular/standalone';
+import { FavoritosService } from 'src/app/services/favoritos/favoritos.service';
+import { IPokemon } from 'src/app/services/pokeapi/pokeapi.mode';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favoritos',
   templateUrl: 'favoritos.page.html',
   styleUrls: ['favoritos.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonAvatar,
+    CommonModule,
+    RouterLink,
+  ],
 })
 export class FavoritosPage {
-  constructor() {}
+  pokemons: IPokemon[] = [];
+
+  constructor(private favoritosService: FavoritosService) {}
+
+  async ionViewWillEnter() {
+    this.pokemons = await this.favoritosService.getFavoritos();
+  }
 }
