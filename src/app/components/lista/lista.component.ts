14741f7f05312cc822d077ade281ee887d669541
Leandro H <ndroher+git@proton.me>
2025-06-18 18:30:02 -0300

    refactor: melhorar exibicao de id e nome dos pokemons

--- src/app/components/lista/lista.component.ts
+++ src/app/components/lista/lista.component.ts
@@ -5,6 +5,7 @@ import {
   IonItem,
   IonLabel,
   IonAvatar,
+  IonText,
 } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 
@@ -17,7 +18,15 @@ export interface IPokemonLista {
   selector: 'app-lista',
   templateUrl: './lista.component.html',
   styleUrls: ['./lista.component.scss'],
-  imports: [IonList, IonItem, IonLabel, IonAvatar, CommonModule, RouterLink],
+  imports: [
+    IonList,
+    IonItem,
+    IonLabel,
+    IonAvatar,
+    IonText,
+    CommonModule,
+    RouterLink,
+  ],
 })
 export class ListaComponent {
   @Input() pokemons: IPokemonLista[] = [];
